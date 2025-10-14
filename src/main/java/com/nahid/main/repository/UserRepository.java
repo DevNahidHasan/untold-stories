cb8b666af389cde60ea4633839b6334d5d7e2250
DevNahidHasan <smnahid.hsn@gmail.com>
2025-10-14 07:25:34 +0600

    Added: repository UserRepository

---
+++ src/main/java/com/nahid/main/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.nahid.main.repository;
+
+import com.nahid.main.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    User findByUsername(String username);
+}
