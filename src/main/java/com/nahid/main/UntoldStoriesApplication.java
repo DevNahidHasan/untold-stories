2a9d8b07fbd3924df4312965080292646581df73
DevNahidHasan <smnahid.hsn@gmail.com>
2025-10-14 06:57:43 +0600

    Added: all the build and app files

---
+++ src/main/java/com/nahid/main/UntoldStoriesApplication.java
@@ -0,0 +1,13 @@
+package com.nahid.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UntoldStoriesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UntoldStoriesApplication.class, args);
+    }
+
+}
