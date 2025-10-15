70d6e274908c9c5be43dcdb177146cebf9c21591
DevNahidHasan <smnahid.hsn@gmail.com>
2025-10-15 07:08:32 +0600

    Added: barebone post structure

--- src/main/java/com/nahid/main/controller/AppController.java
+++ src/main/java/com/nahid/main/controller/AppController.java
@@ -1,13 +1,27 @@
 package com.nahid.main.controller;
 
+import com.nahid.main.model.Story;
+import com.nahid.main.repository.StoryRepository;
+import com.nahid.main.service.StoryService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class AppController {
 
+    private final StoryService storyService;
+
     @GetMapping({"/","/home"})
-    public String homePage(){
+    public String homePage(Model model){
+
+        List<Story> storyList = storyService.getStories();
+        model.addAttribute("storyList",storyList);
+
         return "home-page";
     }
 
